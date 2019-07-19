2741f713bded87038167678d66bbb088acee4a06
Lauren Tallerico <lat13@live.missouristate.edu>
2019-07-18 22:58:11 -0500

    added methods to classes, better formatting w/ using those methods

--- calc_grade.js
+++ calc_grade.js
@@ -1,8 +1,68 @@
 var yourClass;
 
+{ // dataString
+    var dataString = "Lab 06: Debugging\n"
+        + "1.95 / 2.0\n"
+        + "JUL 08JUL 09 AT 11:00PM\n"
+        + "Exam 1, Part 1	\n"
+        + "9.5 / 10.0\n"
+        + "JUL 03JUL 03 AT 11:00AM\n"
+        + "Assignment 03: Dynamic Int Array List	\n"
+        + "25.0 / 28.0\n"
+        + "JUN 24JUL 01 AT 11:00PM\n"
+        + "Lab 05: Classes	\n"
+        + "2.0 / 2.0\n"
+        + "11 Hours, 51 Minutes Late\n"
+        + "JUN 25JUN 26 AT 11:00PM\n"
+        + "LATE DUE DATE: JUN 28 AT 11:00PM\n"
+        + "Assignment 02: Char Stack	\n"
+        + "20.0 / 20.0\n"
+        + "JUN 17JUN 25 AT 11:00PM\n"
+        + "LATE DUE DATE: JUN 27 AT 11:00PM\n"
+        + "Lab 04: Working with Files	\n"
+        + "2.0 / 2.0\n"
+        + "JUN 18JUN 24 AT 11:00PM\n"
+        + "Lab 03: Vectors	\n"
+        + "2.0 / 2.0\n"
+        + "JUN 17JUN 18 AT 11:00PM\n"
+        + "Assignment 01: Developing in C++	\n"
+        + "10.0 / 10.0\n"
+        + "JUN 10JUN 17 AT 11:00PM\n"
+        + "Lab 02: Developing with VS Code	\n"
+        + "2.0 / 2.0\n"
+        + "JUN 11JUN 12 AT 11:00PM\n"
+        + "Lab 01: Hello 232	\n"
+        + "2.0 / 2.0\n"
+        + "JUN 10JUN 11 AT 11:00PM\n"
+        + "Quiz 01: Introduction	\n"
+        + "10.0 / 10.0\n"
+        + "Quiz 03: Structured Data	\n"
+        + "10.0 / 10.0\n"
+        + "Quiz 02: Questions	\n"
+        + "10.0 / 10.0\n"
+        + "Quiz 04: Queues	\n"
+        + "10.0 / 10.0\n"
+        + "Quiz 05: Asymptotic Notation	\n"
+        + "10.0 / 10.0\n"
+        + "Quiz 06: Stacks and Queues	\n"
+        + "10.0 / 10.0\n"
+        + "Quiz 07: Work Day Participation	\n"
+        + "10.0 / 10.0\n"
+        + "Quiz 08: Memory Diagram	\n"
+        + "10.0 / 10.0\n"
+        + "Quiz 09: Rule of Five	\n"
+        + "7.6 / 10.0\n"
+        + "Exam 1, Part 2	\n"
+        + "45.5 / 50.0\n"
+        + "Quiz 10: Attend Guest Lecture	\n"
+        + "10.0 / 10.0\n"
+        + "Quiz 11: Array List	\n"
+        + "10.0 / 10.0\n";
+}
+
 class CategoryInfo {
     constructor(weight) {
-        this.weight = weight;  // category weight
+        this.weight = 100 * weight;  // category weight
         this.yourScore = 0.0;  // your score
         this.maxScore = 0.0;   // potential score
     }
@@ -12,6 +72,16 @@ class CategoryInfo {
     }
     addToMaxScore(score) {
         this.maxScore += score;
+    }
+    getScore() {
+        return (this.yourScore / this.maxScore);
+    }
+    getPercentage() {
+        if (this.isEmpty()) { return 0; }
+        return (this.getScore() * this.weight);
+    }
+    isEmpty() {
+        return this.maxScore == 0.0;
     }
 }
 class CscClass {
@@ -25,6 +95,24 @@ class CscClass {
             let info = new CategoryInfo(this.breakdown[i])
             this.map.set(this.category[i], info);
         }
+    }
+
+    getCurrentScore() {
+        let score = this.getWeightedScore();
+        for (const [key, category] of this.map) {
+            if (category.isEmpty()) {
+                score += category.weight;
+            }
+        }
+        return score;
+    }
+
+    getWeightedScore() {
+        let score = 0.0;
+        for (const [key, category] of this.map) {
+            score += category.getPercentage();
+        }
+        return score;
     }
 }
 
@@ -54,99 +142,64 @@ function classSwitch(choice) {
     yourClass = new CscClass(category, breakdown, string)
 }
 
-
-function getInfo() {
-
-}
-
-var dataString = "Lab 06: Debugging\n"
-    + "1.95 / 2.0\n"
-    + "JUL 08JUL 09 AT 11:00PM\n"
-    + "Exam 1, Part 1	\n"
-    + "9.5 / 10.0\n"
-    + "JUL 03JUL 03 AT 11:00AM\n"
-    + "Assignment 03: Dynamic Int Array List	\n"
-    + "25.0 / 28.0\n"
-    + "JUN 24JUL 01 AT 11:00PM\n"
-    + "Lab 05: Classes	\n"
-    + "2.0 / 2.0\n"
-    + "11 Hours, 51 Minutes Late\n"
-    + "JUN 25JUN 26 AT 11:00PM\n"
-    + "LATE DUE DATE: JUN 28 AT 11:00PM\n"
-    + "Assignment 02: Char Stack	\n"
-    + "20.0 / 20.0\n"
-    + "JUN 17JUN 25 AT 11:00PM\n"
-    + "LATE DUE DATE: JUN 27 AT 11:00PM\n"
-    + "Lab 04: Working with Files	\n"
-    + "2.0 / 2.0\n"
-    + "JUN 18JUN 24 AT 11:00PM\n"
-    + "Lab 03: Vectors	\n"
-    + "2.0 / 2.0\n"
-    + "JUN 17JUN 18 AT 11:00PM\n"
-    + "Assignment 01: Developing in C++	\n"
-    + "10.0 / 10.0\n"
-    + "JUN 10JUN 17 AT 11:00PM\n"
-    + "Lab 02: Developing with VS Code	\n"
-    + "2.0 / 2.0\n"
-    + "JUN 11JUN 12 AT 11:00PM\n"
-    + "Lab 01: Hello 232	\n"
-    + "2.0 / 2.0\n"
-    + "JUN 10JUN 11 AT 11:00PM\n"
-    + "Quiz 01: Introduction	\n"
-    + "10.0 / 10.0\n"
-    + "Quiz 03: Structured Data	\n"
-    + "10.0 / 10.0\n"
-    + "Quiz 02: Questions	\n"
-    + "10.0 / 10.0\n"
-    + "Quiz 04: Queues	\n"
-    + "10.0 / 10.0\n"
-    + "Quiz 05: Asymptotic Notation	\n"
-    + "10.0 / 10.0\n"
-    + "Quiz 06: Stacks and Queues	\n"
-    + "10.0 / 10.0\n"
-    + "Quiz 07: Work Day Participation	\n"
-    + "10.0 / 10.0\n"
-    + "Quiz 08: Memory Diagram	\n"
-    + "10.0 / 10.0\n"
-    + "Quiz 09: Rule of Five	\n"
-    + "7.6 / 10.0\n"
-    + "Exam 1, Part 2	\n"
-    + "45.5 / 50.0\n"
-    + "Quiz 10: Attend Guest Lecture	\n"
-    + "10.0 / 10.0\n"
-    + "Quiz 11: Array List	\n"
-    + "10.0 / 10.0\n";
-
-
-classSwitch("1");
-console.log(yourClass.string);
-var stringArray = dataString.split("\n");
-for (let i = 0; i < stringArray.length; i++) {
-    let wordArray = stringArray[i].split(" ");
-    if (yourClass.category.includes(wordArray[0])
-        || wordArray[0] == "Exam") {
-        // grab wordArray[0]
-        let category = wordArray[0] // for semantics
-        // Exam has 2 entries (Exam1, Exam2)
-        if (wordArray[0] == "Exam") {
-            category += parseInt(wordArray[1]);
+// load info from dataString into CscClass yourClass
+function getInfo(data) {
+    var lineArray = data.split("\n");
+    for (let i = 0; i < lineArray.length; i++) {
+        let wordArray = lineArray[i].split(" ");
+        if (yourClass.category.includes(wordArray[0])
+            || wordArray[0] == "Exam") {
+            // grab wordArray[0]
+            let category = wordArray[0] // for semantics
+            // Exam has 2 entries (Exam1, Exam2)
+            if (wordArray[0] == "Exam") {
+                category += parseInt(wordArray[1]);
+            }
+            // grab lineArray[i+1], parse yourScore and MaxScore from it
+            let scoreArray = lineArray[i + 1].split(" ");
+            let your_score = parseFloat(scoreArray[0]); // for semantics and mutation to number
+            let max_score = parseFloat(scoreArray[2]);  // for semantics and mutation to number
+            // add scores to map
+            yourClass.map.get(category).addToYourScore(your_score);
+            yourClass.map.get(category).addToMaxScore(max_score);
         }
-        // grab stringArray[i+1] parse yourScore and MaxScore from it
-        let scoreArray = stringArray[i + 1].split(" ");
-        let your_score = parseFloat(scoreArray[0]); // for semantics and changing to number
-        let max_score = parseFloat(scoreArray[2]);  // for semantics and changing to number
-        // add scores to map
-        yourClass.map.get(category).addToYourScore(your_score);
-        yourClass.map.get(category).addToMaxScore(max_score);
     }
 }
-yourClass.category.forEach(category => {
-    let formatSpace = (11 - category.length);
-    // add space at end of category name for formatted printing
-    let name = category + " score:" + (" ".repeat(formatSpace));
-    let score = yourClass.map.get(category).yourScore
-        + "/" + yourClass.map.get(category).maxScore;
-    score = score + (" ".repeat(10 - score.length)); // formatted printing
-    console.log(name + score
-        + "  --> weight: " + yourClass.map.get(category).weight * 100 + "%")
-});
+
+// printing formatted information
+function print() {
+    console.log("\n" + yourClass.string);
+    console.log("*************************************************************************");
+    console.log("Category     Your Score   Percent%");
+    console.log("-----------------------------------");
+    for (const [name, category] of yourClass.map) {
+        let title = name + ":" + (" ".repeat(12 - name.length));
+        let your_score = category.isEmpty() ? "    0" : category.yourScore.toPrecision(4);
+        let max_score = category.isEmpty() ? "0    " : category.maxScore.toPrecision(4);
+        let percentage = category.isEmpty() ? "    0" : category.getPercentage().toPrecision(4);
+        console.log(title
+            + your_score
+            + "/" + max_score + "   "
+            + percentage + "%");
+    }
+    console.log("-----------------------------------");
+    console.log("Current Score:             " + yourClass.getCurrentScore().toPrecision(4) + "%");
+    console.log("Weighted Score:            " + yourClass.getWeightedScore().toPrecision(4) + "%");
+    console.log("*************************************************************************");
+    console.log("\nNOTE:\n"
+        + "Weighted grade does not include future material.\n"
+        + "It only reflects what you have earned thus far.\n"
+        + "To have 100%, you must have all material completed.\n"
+        + "For example, exam 2 would not be included until your last day of class.\n"
+        + "\nGrade breakdown for this class:\n");
+    for (const [name, category] of yourClass.map) {
+        let title = name + ":" + (" ".repeat(12 - name.length));
+        let ifIncomplete = category.isEmpty() ? " Not yet completed." : "";
+        console.log(title + category.weight + "%" + ifIncomplete);
+    }
+}
+
+// testing program
+classSwitch("1");
+getInfo(dataString);
+print();
